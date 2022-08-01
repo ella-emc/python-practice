13aad95f93011f3d9df5ffef7a1dff94594d14bd
blossomstacks <ellalala.mc@gmail.com>
2022-08-01 22:01:58 +0800

    Create bmi-evaluator.py

---
+++ bmi-evaluator.py
@@ -0,0 +1,22 @@
+print("Body Mass Index Calculator and Evaluator")
+print("For this to work, type in your weight in kilograms and height in meters below.")
+weight = float(input("Weight in Kilograms: "))
+height = float(input("Height in Meters: "))
+
+bmi = weight // height ** 2
+print("Your body mass index (BMI) is " + str(bmi))
+
+if bmi < 18.5:
+    print("Your BMI classification is Underweight.")
+if bmi >= 18.5 and bmi <= 24.9:
+    print("Your BMI classification is Normal.")
+if bmi >= 25.0 and bmi <= 29.9:
+    print("Your BMI classification is Overweight.")
+if bmi >= 30.0 and bmi <= 34.9:
+    print("Your BMI classification is Obese, Class I.")
+if bmi >= 35.0 and bmi <= 39.9:
+    print("Your BMI classification is Obese, Class II.")
+if bmi >= 40:
+    print("Your BMI falls under Extreme Obesity.")
+
+print("The BMI classifications above were retrieved from the National Heart, Lung, and Blood Institute.")
